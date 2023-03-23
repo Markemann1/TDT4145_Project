cb1e90fa5f5679cc72e4d56c5a45b83320db905e
gunvorsh <gunvorsh@gmail.com>
2023-03-23 12:52:11 +0100

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sqlite3
 
-#Kjører funksjonaliteten:
-interface()
+
 
 
 #Funksjonalitet som skal programmeres:
@@ -25,20 +24,20 @@ def interface():
         "2. Søke etter togruter mellom to stasjoner \n"+
         "3. Logge inn i kunderegisteret")
     valg=input("Skriv inn et tall fra 1-3: ")
-    if valg==1:
-        togrute_dag_stasjon()
-    elif valg==2:
+    if valg=="1":
+        togrute_dag_stasjon() 
+    elif valg=="2":
         togrute_to_stasjoner()
-    elif valg==3:
-        kunderegister()
+ #   elif valg=="3":
+ #       kunderegister()
 
 
 def togrute_dag_stasjon():
     st=input("Skriv inn ønsket stasjon: ")
     dg=input("Skriv inn ønsket ukedag: ")
-    con = sqlite3.connect("togdatabase.sql")
+    con = sqlite3.connect("../togdatabase.db")  
     cursor=con.cursor()
-    cursor.execute(f"SELECT * FROM Togrute INNER JOIN Delstrekning WHERE ukedag={dg} AND (startstasjon={st} OR endestasjon={st})")
+    cursor.execute(f"SELECT DISTINCT Togrute.Rutenavn FROM Togrute INNER JOIN Delstrekning WHERE Avgangsdag='{dg}' AND (Delstrekning.Startstasjon='{st}' OR Delstrekning.Endestasjon='{st}')")
     rows=cursor.fetchall()
     con.close()
     print("Følgende togruter ble funnet:")
@@ -48,16 +47,20 @@ def togrute_dag_stasjon():
 def togrute_to_stasjoner():
     st1=input("Skriv inn ønsket startstasjon: ")
     st2=input("Skriv inn ønsket endestasjon: ")
-    con = sqlite3.connect("togdatabase.sql")
+    con = sqlite3.connect("../togdatabase.db")
     cursor=con.cursor()
-    cursor.execute(f"SELECT * FROM Togrute INNER JOIN Delstrekning WHERE ukedag={dg} AND (startstasjon={st} OR endestasjon={st})")
+    cursor.execute(f"SELECT * FROM Togrute INNER JOIN Delstrekning WHERE (Delstrekning.startstasjon='{st1}' AND Delstrekning.endestasjon='{st2}') OR (Delstrekning.startstasjon='{st2}' AND Delstrekning.endestasjon='{st1}')")
     rows=cursor.fetchall()
     con.close()
     print("Følgende togruter ble funnet:")
     print(rows)
 
 
-def kunderegister():
+#def kunderegister():
     #Spørre om kunden vil logge inn eller registrere seg
     #Mulighet for å kjøpe billett
     #Mulighet for å se eksisterende kjøp
+
+
+#Kjører funksjonaliteten:
+interface()
